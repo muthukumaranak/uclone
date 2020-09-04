7c1cfd2272927d7a0167cd3a4fcfad7906662885
Muthukumaran G <kumaranoc@gmail.com>
2020-09-04 21:56:31 +0530

    Tested with dummy thymeleaf ui passing model object

--- src/main/java/com/app/youtubeclone/controller/MediaStatusController.java
+++ src/main/java/com/app/youtubeclone/controller/MediaStatusController.java
@@ -1,19 +1,30 @@
 package com.app.youtubeclone.controller;
 
 
+import com.app.youtubeclone.entity.MediaComment;
+import com.app.youtubeclone.entity.MediaFile;
+import com.app.youtubeclone.repository.MediaFileRepo;
 import com.app.youtubeclone.service.MediaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+import java.util.List;
 
-@RestController
+
+@Controller
 public class MediaStatusController {
 
     @Autowired
     MediaService mediaService;
+
+    @Autowired
+    MediaFileRepo mediaFileRepo;
 
     @PostMapping("/comment")
     public String comment(@RequestParam String id, @RequestParam String comment){
@@ -33,5 +44,31 @@ public class MediaStatusController {
     @PostMapping("/views")
     public String views(@RequestParam String id){
         return mediaService.views(Integer.parseInt(id));
+    }
+
+    @GetMapping("/receiveall")
+    public String retrieve(Model model) {
+        List<MediaFile> listDTOS = new LinkedList<>();
+        List<MediaFile> list = mediaFileRepo.findAll();
+        list.forEach(blogPost -> {
+            MediaFile blogPostDTO = new MediaFile();
+            blogPostDTO.setId(blogPost.getId());
+            blogPostDTO.setOwner(blogPost.getOwner());
+            blogPostDTO.setThumbnailUrl(blogPost.getThumbnailUrl());
+            blogPostDTO.setVideoUrl(blogPost.getVideoUrl());
+            List<MediaComment> commentDTOS = new LinkedList();
+            blogPost.getMediaComment().forEach(comment -> {
+                MediaComment commentDTO = new MediaComment();
+                commentDTO.setCommentby(comment.getCommentby());
+                commentDTO.setComment(comment.getComment());
+                commentDTO.setId(comment.getId());
+                commentDTOS.add(commentDTO);
+            });
+            blogPostDTO.setMediaComment(commentDTOS);
+            listDTOS.add(blogPostDTO);
+        });
+        model.addAttribute("list",listDTOS);
+        return "ssss";
+        //return listDTOS;
     }
 }
